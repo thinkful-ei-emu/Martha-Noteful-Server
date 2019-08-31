9b44f574442a90d6c4b1f42ea4950a073d0ed7b9
Martha Plantz <maplantz89@gmail.com>
2019-08-30 17:09:39 -0700

    cors

--- src/app.js
+++ src/app.js
@@ -16,14 +16,14 @@ const morganOptions = (NODE_ENV === 'production')
 
 app.use(morgan(morganOptions));
 app.use(helmet());
-// app.use(cors());
-// app.use(function(req, res, next) { 
-//   res.header("Access-Control-Allow-Origin", '*'); 
-//   res.header("Access-Control-Allow-Credentials", true); 
-//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS'); 
-//   res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json'); 
-//   next(); 
-// });
+//app.use(cors());
+app.use(function(req, res, next) { 
+  res.header("Access-Control-Allow-Origin", '*'); 
+  res.header("Access-Control-Allow-Credentials", true); 
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS'); 
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json'); 
+  next(); 
+});
 
 app.use('/api/folders', foldersRouter);
 app.use('/api/notes', notesRouter);
